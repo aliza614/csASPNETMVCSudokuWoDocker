50f6a5d4daf19f63c76877c24644790fe79431c0
Aliza OConnor <alizaoconnor1@yahoo.com>
2022-07-11 11:16:44 -0500

    initial commit

---
+++ csASPNETMVCSudokuWoDocker/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace csASPNETMVCSudokuWoDocker.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
